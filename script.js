86f58cea4c84ef787c0eda6aceb72c562bf62db8
Ali nawaz <167767716+alinawz0@users.noreply.github.com>
2025-02-16 22:00:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+let navLinks = document.querySelectorAll(".nav-menu .nav-link");
+let menuOpenButton = document.querySelector("#menu-open-button");
+let menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () =>{
+    document.body.classList.toggle("show-mobile-menu");
+})
+
+menuCloseButton.addEventListener("click", () => menuOpenButton.click()
+);
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () =>  menuOpenButton.click());
+});
+
+const swiper = new Swiper('.slider-wrapper', {
+   
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1034: {
+            slidesPerView: 3
+        },
+
+    }
+    
+  });
